d8eb05170164812938a366fa82206e9dbec75d62
Sarah <salibay@bi-consulting.com>
2020-07-03 22:22:53 +0200

    New projet

---
+++ main.js
@@ -0,0 +1,7 @@
+let newDestination = document.createElement('li');
+
+newDestination.id = 'oaxaca';
+
+newDestination.innerHTML = 'Oaxaca, Mexico';
+
+document.getElementById('more-destinations').appendChild(newDestination);
